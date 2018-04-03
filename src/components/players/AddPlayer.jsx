b444eb4564bd950a14db723539c67786a7918229
Tim Atherton <taweb@github.com>
2018-04-03 11:13:54 +0100

    add player input component

---
+++ src/components/players/AddPlayer.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Input from './Input';
+
+class AddPlayer extends Component {
+	constructor(props) {
+		super(props);
+		this.onChange = this.onChange.bind(this);
+		this.state = {
+			value: ""
+		}
+	}
+
+	onChange(e) {
+		this.setState({
+			value: e.target.value,
+		});
+	}
+
+	render() {
+		return (
+		<form onSubmit={this.onSubmit}>
+			<Input onChange={this.onChange} value={this.state.value} />
+			<button disabled={this.state.value.length < 3}>Add</button>
+		</form>
+		);
+	}
+}
+
+export default AddPlayer;
--- src/components/players/CollectPlayers.jsx
+++ src/components/players/CollectPlayers.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import AddPlayer from './AddPlayer';
+
 const CollectPlayers = () => (
 	<div>
 		<p>
 			<Link to={"/tournament"}>Tournament</Link>
 		</p>
+		<AddPlayer />
 	</div>
 );
 
