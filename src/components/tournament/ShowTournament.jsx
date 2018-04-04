554cf9cc5231096546e18ac9bd175c6b5c173b5f
Tim Atherton <taweb@github.com>
2018-04-04 17:39:14 +0100

    add ShowTournament container, get rounds as props to component

--- src/components/tournament/ShowTournament.jsx
+++ src/components/tournament/ShowTournament.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ShowTournament = () => (
-	<div>
-		<p>
-			<Link to={"/"}>CollectPlayers</Link>
-		</p>
-	</div>
-);
+const ShowTournament = ({rounds}) => {
+
+	console.log(rounds);
+ 
+	return (
+		<div>
+			{rounds === undefined ?
+				<div>
+					<p>It seems you have visited the tournament page prematurely, please follow the link and enter your team</p>
+					<Link to={"/"}>CollectPlayers</Link>
+				</div>
+			:null}
+		</div>
+	);
+}
 
 export default ShowTournament;
